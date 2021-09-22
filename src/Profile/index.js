8f89832dd653a321e305f76a1d392bfa02d5d77f
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2021-09-22 21:12:55 +0300

    add radioButtons to Profile component

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,13 +1,47 @@
+import * as React from 'react';
 import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormLabel from '@mui/material/FormLabel';
+import Radio from '@mui/material/Radio';
 
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+// const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 const Profile = () => {
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
   return  <div>
-  <Checkbox {...label} defaultChecked />
-  <Checkbox {...label} />
-  <Checkbox {...label} />
-  <Checkbox {...label} />
+    <div>Name: Nikita</div>
+
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Gender</FormLabel>
+      <RadioGroup
+        aria-label="gender"
+        defaultValue="Other"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel value="Woman" control={<Radio />} label="Woman" />
+        <FormControlLabel value="Man" control={<Radio />} label="Man" />
+        <FormControlLabel value="Other" control={<Radio />} label="Other" />
+      </RadioGroup>
+    </FormControl>
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Are you over 18?</FormLabel>
+      <RadioGroup
+        aria-label="Age"
+        defaultValue="No"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="No" control={<Radio />} label="No" />
+      </RadioGroup>
+    </FormControl> 
 </div>;
 };
 
