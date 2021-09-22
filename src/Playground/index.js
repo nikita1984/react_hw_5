8606169bec9a1f625e6e5068c10be1d514e3e4ee
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2021-09-22 20:34:20 +0300

    add checkbox to Profile component

--- src/Playground/index.js
+++ src/Playground/index.js
@@ -1,9 +1,9 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-import { useSelector, useDispatch } from "react-redux";
-import { MyDataContext } from "../index";
+import { useSelector } from "react-redux";
+// import { MyDataContext } from "../index";
 
 const useStyles = makeStyles(() => ({
   noUnderline: {
--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,5 +1,14 @@
-const Profile = (props) => {
-  return <div>Profile page</div>;
+import Checkbox from '@mui/material/Checkbox';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+const Profile = () => {
+  return  <div>
+  <Checkbox {...label} defaultChecked />
+  <Checkbox {...label} />
+  <Checkbox {...label} />
+  <Checkbox {...label} />
+</div>;
 };
 
 export default Profile;
